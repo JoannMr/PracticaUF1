83d594bad2f41ae2d3cac8c299ab404dc63b8cc0
JoannMr <joanmerino120205@gmail.com>
2023-11-17 15:40:46 +0100

    Primer programa de C#

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hola, este es mi humilde programa en C#");
+
+        MostrarMensajePersonalizado();
+
+        Console.WriteLine("¿Cuál es tu nombre?");
+        string nombreUsuario = Console.ReadLine();
+
+        SaludarUsuario(nombreUsuario);
+    }
+
+    static void MostrarMensajePersonalizado()
+    {
+        Console.WriteLine("Este programa necesita tus datos.");
+    }
+
+    static void SaludarUsuario(string nombre)
+    {
+        Console.WriteLine($"¡Hola, {nombre}! Bienvenido al programa.");
+    }
+}
